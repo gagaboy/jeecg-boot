c6ba34d9485ac775407b6261e69e5483c034d4f7
Eric Chang <gagaboy@126.com>
2021-06-29 10:01:33 +0800

    前端切换浏览器功能注释

--- ant-design-vue-jeecg/src/App.vue
+++ ant-design-vue-jeecg/src/App.vue
@@ -17,23 +17,23 @@
     },
     created () {
       let that = this
-      enquireScreen(deviceType => {
-        // tablet
-        if (deviceType === 0) {
-          that.$store.commit('TOGGLE_DEVICE', 'mobile')
-          that.$store.dispatch('setSidebar', false)
-        }
-        // mobile
-        else if (deviceType === 1) {
-          that.$store.commit('TOGGLE_DEVICE', 'mobile')
-          that.$store.dispatch('setSidebar', false)
-        }
-        else {
-          that.$store.commit('TOGGLE_DEVICE', 'desktop')
-          that.$store.dispatch('setSidebar', true)
-        }
+      // enquireScreen(deviceType => {
+      //   // tablet
+      //   if (deviceType === 0) {
+      //     that.$store.commit('TOGGLE_DEVICE', 'mobile')
+      //     that.$store.dispatch('setSidebar', false)
+      //   }
+      //   // mobile
+      //   else if (deviceType === 1) {
+      //     that.$store.commit('TOGGLE_DEVICE', 'mobile')
+      //     that.$store.dispatch('setSidebar', false)
+      //   }
+      //   else {
+      //     that.$store.commit('TOGGLE_DEVICE', 'desktop')
+      //     that.$store.dispatch('setSidebar', true)
+      //   }
 
-      })
+      // })
     }
   }
 </script>
